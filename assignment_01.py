968188d601a8e6ee3f09c52bd452a0904a108fb7
surya987-cmd <kurupsurya987@gmail.com>
2026-02-20 12:57:45 +0530

    Add files via upload

---
+++ assignment_01.py
@@ -0,0 +1,23 @@
+#Task 1
+
+a=int(input("Enter the first number:"))
+b=int(input("Enter the second number:"))
+add=(a+b)
+dif=(a-b)
+pro=(a*b)
+div=round((a/b),2)
+print("Addition:",add)
+print("Subtraction:",dif)
+print("Multiplication:",pro)
+print("Division:",div)
+
+
+#Task 2
+
+f_nam=input("Enter your first name:")
+l_nam=input("Enter your last name:")
+name=f_nam +l_nam+'!'
+print("Hello,",name,"Welcome to the Python program.")
+
+
+
